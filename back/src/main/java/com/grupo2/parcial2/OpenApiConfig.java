53b3952bafb48709ca1b6185ac80fd91aa015de5
Emanuel M <emanuel.munera1@gmail.com>
2024-11-29 12:23:13 -0500

    Swagger done

---
+++ back/src/main/java/com/grupo2/parcial2/OpenApiConfig.java
@@ -0,0 +1,17 @@
+package com.grupo2.parcial2;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API de Gestión de Notas")
+
+                        .version("1.0")
+                        .description("Documentación de la API de notas de estudiante Parcial 2"));
+    }
+}
--- back/src/main/java/com/grupo2/parcial2/model/StudentSubject.java
+++ back/src/main/java/com/grupo2/parcial2/model/StudentSubject.java
@@ -1,6 +1,6 @@
 package com.grupo2.parcial2.model;
 
-import jakarta.persistence.*;
+import javax.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
